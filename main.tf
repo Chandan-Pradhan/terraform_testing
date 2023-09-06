a3ef7c52d844dfbe3455bc03cb37102e235d94a4
Chandan Pradhan <42320996+Chandan-Pradhan@users.noreply.github.com>
2023-09-06 10:39:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-resource "google_storage_bucket" "auto_expire" {
-  name          = "no-public-access-bucket"
-  location      = "US"
-  force_destroy = true
-
-  public_access_prevention = "enforced"
+resource "google_compute_network" "vpc_network" {
+  name = "vpc-network"
 }
